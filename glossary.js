0173b14d4df15ac327f65fa756cf25a31aeb65ce
Jeanaica Suplido <jeanaica.suplido@phitopolis.com>
2022-02-24 16:09:29 +0800

    WIP

---
+++ glossary.js
@@ -0,0 +1,166 @@
+const glossary = [
+  {
+    file: 'YYYYMMDD.nyse_xdp_univ.rev.csv.gz',
+    file_set_type: 'raw_data',
+    fields: [
+      {
+        field: 'bb_global_id',
+        data_type: 'str',
+        description: 'sample'
+      },
+      {
+        field: 'bb_bsid',
+        data_type: 'str',
+        description: 'sample'
+      },
+      {
+        field: 'system_id',
+        data_type: 'str',
+        description: 'sample'
+      },
+      {
+        field: 'price_scale_code',
+        data_type: 'str',
+        description: 'sample'
+      },
+      {
+        field: 'uot',
+        data_type: 'str',
+        description: 'sample'
+      },
+      {
+        field: 'ticket_designation',
+        data_type: 'str',
+        description: 'sample'
+      },
+      {
+        field: 'list_mkt',
+        data_type: 'str',
+        description: 'sample'
+      },
+      {
+        field: 'nyse_trade_mkt',
+        data_type: 'str',
+        description: 'sample'
+      },
+      {
+        field: 'symbol_index',
+        data_type: 'str',
+        description: 'sample'
+      },
+      {
+        field: 'cqs_ticker',
+        data_type: 'str',
+        description: 'sample'
+      },
+      {
+        field: 'nyse_ticker',
+        data_type: 'str',
+        description: 'sample'
+      },
+      {
+        field: 'sid',
+        data_type: 'str',
+        description: 'sample'
+      },
+      {
+        field: 'mpv',
+        data_type: 'str',
+        description: 'sample'
+      },
+      {
+        field: 'is_test_symbol',
+        data_type: 'str',
+        description: 'sample'
+      },
+       {
+        field: 'round_lot',
+        data_type: 'str',
+        description: 'sample'
+      },
+    ]
+  },
+   {
+    file: 'YYYYMMDD.memo_xdp_univ.rev.csv.gz',
+    file_set_type: 'raw_data',
+    fields: [
+      {
+        field: 'global_id',
+        data_type: 'str',
+        description: 'sample'
+      },
+      {
+        field: 'bsid',
+        data_type: 'str',
+        description: 'sample'
+      },
+      {
+        field: 'system_id',
+        data_type: 'str',
+        description: 'sample'
+      },
+      {
+        field: 'price_scale_code',
+        data_type: 'str',
+        description: 'sample'
+      },
+      {
+        field: 'uot',
+        data_type: 'str',
+        description: 'sample'
+      },
+      {
+        field: 'ticket_designation',
+        data_type: 'str',
+        description: 'sample'
+      },
+      {
+        field: 'list_mkt',
+        data_type: 'str',
+        description: 'sample'
+      },
+      {
+        field: 'nyse_trade_mkt',
+        data_type: 'str',
+        description: 'sample'
+      },
+      {
+        field: 'symbol_index',
+        data_type: 'str',
+        description: 'sample'
+      },
+      {
+        field: 'cqs_ticker',
+        data_type: 'str',
+        description: 'sample'
+      },
+      {
+        field: 'nyse_ticker',
+        data_type: 'str',
+        description: 'sample'
+      },
+      {
+        field: 'sid',
+        data_type: 'str',
+        description: 'sample'
+      },
+      {
+        field: 'mpv',
+        data_type: 'str',
+        description: 'sample'
+      },
+      {
+        field: 'is_test_symbol',
+        data_type: 'str',
+        description: 'sample'
+      },
+       {
+        field: 'round_lot',
+        data_type: 'str',
+        description: 'sample'
+      },
+    ]
+  },
+]
+
+module.exports = glossary
